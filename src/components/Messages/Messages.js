9d9490192b576c7d8ad8b810eef91093c2e4aa6a
gchan82 <42255277+gchan82@users.noreply.github.com>
2018-10-22 18:46:56 -0700

    End of Lecture 27

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,8 +1,22 @@
 import React, { Component } from "react";
+import { Segment, Comment } from 'semantic-ui-react';
+import MessagesHeader from './MessagesHeader';
+import MessagesForm from './MessagesForm';
 
 export class Messages extends Component {
   render() {
-    return <div>Messages Messages</div>;
+    return (
+      <React.Fragment>
+        <MessagesHeader />
+
+        <Segment>
+          <Comment.Group className="messages">
+          {/* Messages*/}
+          </Comment.Group>
+        </Segment>
+        <MessagesForm />
+      </React.Fragment>
+    );
   }
 }
 
