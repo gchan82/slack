8f536811f87da2adb4bb7d05c40d1baa6c92c7e8
gchan82 <42255277+gchan82@users.noreply.github.com>
2018-10-22 20:07:32 -0700

    End of Lecture 28

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,20 +1,29 @@
 import React, { Component } from "react";
-import { Segment, Comment } from 'semantic-ui-react';
-import MessagesHeader from './MessagesHeader';
-import MessagesForm from './MessagesForm';
+import { Segment, Comment } from "semantic-ui-react";
+import firebase from "../../firebase";
+import MessagesHeader from "./MessagesHeader";
+import MessagesForm from "./MessagesForm";
 
 export class Messages extends Component {
+  state = {
+    messagesRef: firebase.database().ref("messages"),
+    channel: this.props.currentChannel,
+    user: this.props.currentUser
+  };
   render() {
+    const { messagesRef, channel, user } = this.state;
     return (
       <React.Fragment>
         <MessagesHeader />
 
         <Segment>
-          <Comment.Group className="messages">
-          {/* Messages*/}
-          </Comment.Group>
+          <Comment.Group className="messages">{/* Messages*/}</Comment.Group>
         </Segment>
-        <MessagesForm />
+        <MessagesForm 
+        messagesRef={messagesRef} 
+        currentChannel={channel}
+        currentUser={user}
+        />
       </React.Fragment>
     );
   }
